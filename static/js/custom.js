e9d3df80954be65d117cd3e5577cb3187913ce33
Bogdan Staicu <bogdan.staicu@avira.com>
2016-06-26 23:29:58 +0300

    updated img size limit + img size restriction

--- static/js/custom.js
+++ static/js/custom.js
@@ -204,7 +204,7 @@ $(document).ready(function () {
                 dataType: 'json',
                 // autoUpload: false,
                 acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-                maxFileSize: 999000,
+                maxFileSize: 9990000,
                 limitMultiFileUploads: 1,
                 // Enable image resizing, except for Android and Opera,
                 // which actually support image resizing, but fail to
